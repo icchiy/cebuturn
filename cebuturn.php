e9c183c5813b957d82507c432bd4f64ef2d1a6ae
icchiy <icchi050502@gmail.com>
2015-12-04 16:18:01 +0800

    ファーストコミット

---
+++ cebuturn.php
@@ -0,0 +1,84 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+	<head>
+<meta http-equiv="UTF-8">
+	  <title>ぱくりセブターン</title>
+	 <!-- CSS -->
+	  <link rel="stylesheet" href="assets/css/bootstrap.css">
+	  <link rel="stylesheet" href="assets/font-awesome/css/font-awesome.css">
+	  <link rel="stylesheet" href="assets/css/form.css">
+	  <link rel="stylesheet" href="assets/css/timeline.css">
+	  <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<div class="container">
+<header>
+	<a href="http://intern-college.com/"><img class="grid_3 alpha" id="g_logo" src="http://intern-college.com/wp-content/themes/wordpress_cebtern%202/images/logo_new.png" data-pin-nopin="true"></a>
+	<p class="">フィリピン・セブ島No.1の海外インターンシップ求人サイト</p>
+	<div class="contact">
+		<p>お問い合わせ</p>
+	</div>
+
+</header>
+<body>
+
+<div class="contents-1"><a class="slide" href="http://intern-college.com/archives/1716/"><img src="http://intern-college.com/wp-content/themes/wordpress_cebtern%202/images/banner/soudankai_new.jpg"></a>
+</div>
+<div class="contents-2">
+<ul>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+<li>
+<i class="fa fa-home"></i><br>
+<a href="http://intern-college.com/">HOME</a>
+</li>
+</ul>
+</div>
+<div class="contents-3">
+<h1>新着インターンシップ情報</h1>
+</div>
+<div class="contents-4">
+	<div class="contents-4-top">
+<img width="280" height="176" src="img/ダウンロード (1).png" class="" alt="">
+</div>
+<div class="contents-4-4">
+	<a href="http://intern-college.com/archives/intern/interview29/">企業 No.29</a>
+	<a id="f_business" href="http://intern-college.com/archives/intern/interview29/">不動産</a>
+	<div class="contents-4-4">
+			<a class="primary_btn" href="http://intern-college.com/archives/intern/interview29/">インタビュー記事を見る</a>
+	</div>
+</div>
+</div>
+<div class="contents-5"></div>
+<div class="contents-6"></div>
+<div class="contents-7"></div>
+<div class="contents-8"></div>
+</div><!-- container -->
+</body>
+
+</html>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <!-- Include all compiled plugins (below), or include individual files as needed -->
+  <script src="assets/js/bootstrap.js"></script>
+  <script src="assets/js/form.js"></script>
